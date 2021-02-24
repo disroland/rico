92db384ac37950bf00f98083ee28d083027eb9b9
Eugene <disroland@gmail.com>
2021-02-24 18:32:21 +0200

    'mobile'

--- js/app.js
+++ js/app.js
@@ -1,13 +1,22 @@
 'use strict'
 
-let navTabs = document.querySelector('.nav-tabs');
+let navTabs = document.querySelectorAll('.nav-tabs');
 let tabBlocks = document.querySelectorAll('.tab-block');
 let navItems = document.querySelectorAll('.nav-item');
+let navItems2 = document.querySelectorAll('.nav-item2');
+let menuBtn = document.querySelector('.menu-btn');
+let hiddenMnu = document.querySelector('.hidden_menu');
 
 
+for (let i=0; i<navTabs.length; i++){
+navTabs[i].addEventListener("click", showTabs)
+}
 
-
-navTabs.addEventListener("click", showTabs)
+menuBtn.addEventListener('click', function(e){
+	hiddenMnu.classList.toggle('show_hidden_menu');
+	menuBtn.classList.toggle('pressed')
+	}
+	)
 
 
 function showTabs(e){
@@ -15,10 +24,19 @@ function showTabs(e){
 	for (let i=0; i<tabBlocks.length;i++){
 		if (tabBlocks[i].classList.contains('active') && tabBlocks[i].getAttribute('data-tab') !== e.target.getAttribute('data-nav')){
 			tabBlocks[i].classList.remove('active', 'show');
-			navItems[i].classList.remove('active', 'show')
+			navItems[i].classList.remove('active', 'show');
+			navItems2[i].classList.remove('active', 'show')
 		} else if (tabBlocks[i].getAttribute('data-tab') === e.target.getAttribute('data-nav')){
 			tabBlocks[i].classList.add('active', 'show');
-			navItems[i].classList.add('active', 'show')
+			navItems[i].classList.add('active', 'show');
+			navItems2[i].classList.add('active', 'show')
 		}
 	}
 }
+
+window.addEventListener('resize', function(){
+	
+		hiddenMnu.classList.remove('show_hidden_menu');
+	    menuBtn.classList.remove('pressed')
+	
+})
